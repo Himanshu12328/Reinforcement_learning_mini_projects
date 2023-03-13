edea8054ca01a6d3403c41d480681f2c91f2f624
Himanshu Gautam <31892893+Himanshu12328@users.noreply.github.com>
2023-03-13 02:32:52 -0400

    Add files via upload

---
+++ Breakout/utils.py
@@ -0,0 +1,13 @@
+import torch
+import numpy as np
+
+
+def tensor(x):
+    if isinstance(x, torch.Tensor):
+        return x
+    x = np.asarray(x, dtype=np.float)
+    if torch.cuda.is_available():
+        x = torch.tensor(x, device=torch.device(0), dtype=torch.float32)
+    else:
+        x = torch.tensor(x, device=torch.device('cpu'), dtype=torch.float32)
+    return x
