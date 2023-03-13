a2b973e2685e929fe85498acef1aeed39e1ded8c
Himanshu Gautam <31892893+Himanshu12328@users.noreply.github.com>
2023-03-13 02:45:30 -0400

    Update agent.py

--- Robotic_Grasping_with_Deep_Reinforcement_Learning/src/agent.py
+++ Robotic_Grasping_with_Deep_Reinforcement_Learning/src/agent.py
@@ -1,1 +1,15 @@
+class Agent(object):
+    def __init__(self, env):
+        self.env = env
 
+    def make_action(self, observation, test=True):
+        """
+        Return predicted action of your agent from observation
+        """
+        raise NotImplementedError("Subclasses should implement this!")
+
+    def init_game_setting(self):
+        """
+        Init things for test time
+        """
+        raise NotImplementedError("Subclasses should implement this!")
